69cc63ebb3e78ef9839ddab08de9746d82e6503e
sh4h4 <abrorhalimov29@gmail.com>
2022-01-20 18:08:56 +0500

    tayor

--- js/main.js
+++ js/main.js
@@ -260,3 +260,18 @@
 
 })(jQuery);
 
+
+// Fetch all details element
+const details = Array.from(document.querySelectorAll("details"));
+
+// Add onclick listeners
+details.forEach(targetDetail => {
+  targetDetail.addEventListener("click", () => {
+    // Close all details that are not targetDetail
+    details.forEach(detail => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
